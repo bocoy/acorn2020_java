3c349f8da6725b04428c7ee262ab2886cad11500
thomas819 <thomass0819@gmail.com>
2020-06-01 14:39:14 +0900

    object project 추가함

---
+++ Step05_Mehod/src/test/main/MainClass02.java
@@ -0,0 +1,13 @@
+package test.main;
+
+import test.mypac.Car;
+import test.mypac.MyObject;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		MyObject obj1 = new MyObject();
+		Car a = obj1.getCar();
+		Car b = obj1.getCar();
+		Car c = obj1.getCar();
+	}
+}
---
+++ Step05_Mehod/src/test/main/MainClass03.java
@@ -0,0 +1,14 @@
+package test.main;
+
+import test.mypac.Car;
+import test.mypac.MyObject;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		MyObject obj1 = new MyObject();
+		obj1.setNum(10);
+		obj1.setName("김구라");
+		obj1.useCar(new Car());
+	}
+	
+}
